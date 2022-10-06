9bd119860d6013271f9aefa3de4717e857320d6e
PennyWizard <104772721+PennyWizard@users.noreply.github.com>
2022-10-06 18:19:32 -0400

    Player Movement
    First-Person Controls
    Jump / Walk
    First-Person Camera
    Player Prefab

--- Terrific Crusaders Shooter/Assets/Scripts/PlayerController.cs
+++ Terrific Crusaders Shooter/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    [SerializeField] CharacterController controller;
+
+    [SerializeField] float playerSpeed;
+    [SerializeField] float jumpHeight;
+    [SerializeField] float gravityValue;
+
+    [SerializeField] int jumpMax;
+
+    private Vector3 playerVelocity;
+    private int timesJumped;
+    
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        movement();
+       
     }
+
+    void movement()
+    {
+        //is on the ground, reset Y velocity, reset jump counter
+        if (controller.isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+            timesJumped = 0;
+        }
+
+        //first-person get inputs for ground movement
+        Vector3 move = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical");
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        // Jumping
+        if (Input.GetButtonDown("Jump") && timesJumped < jumpMax)
+        {
+            timesJumped++;
+            playerVelocity.y = jumpHeight;
+        }
+
+        //Gravity
+        playerVelocity.y -= gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+
 }
---
+++ Terrific Crusaders Shooter/Assets/Scripts/cameraControlls.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControlls : MonoBehaviour
+{
+    [SerializeField] int sensHort;
+    [SerializeField] int sensVert;
+
+    [SerializeField] int lockVertMin;
+    [SerializeField] int lockVertMax;
+
+    [SerializeField] bool invert;
+
+    float xRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //get input
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * sensHort;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * sensVert;
+
+        if(invert)
+            xRotation += mouseY;
+        else
+            xRotation -= mouseY;
+
+        //clamp camera rotation
+        xRotation = Mathf.Clamp(xRotation, lockVertMin, lockVertMax);
+
+        // rotate the camera on the x-axis
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        // rotate player
+        transform.parent.Rotate(Vector3.up * mouseX);
+    }
+}
