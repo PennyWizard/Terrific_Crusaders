dd1dd168cd364e7a8932e8c05d5e540397c2b611
PennyWizard <104772721+PennyWizard@users.noreply.github.com>
2022-10-08 15:27:48 -0400

    I need a gun model
    I finished work on the game manager for today.
    
    I need a gun model to add the gun stats to, for further testing.

--- Terrific Crusaders Shooter/Assets/Scripts/GameManager.cs
+++ Terrific Crusaders Shooter/Assets/Scripts/GameManager.cs
@@ -1,18 +1,93 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
+    public int enemyNumber;
+
+    [Header("---Player Stuff---")]
+    public GameObject player;
+    public PlayerController playerScript;
+    public GameObject spawnPosition;
+
+    [Header("---UI---")]
+    public GameObject pauseMenu;
+    public GameObject playerDeadMenu;
+    public GameObject winMenu;
+    public GameObject menuCurrentlyOpen;
+    public GameObject playerDamageFlash;
+    public Image playerHPBar;
+    public TextMeshProUGUI enemyCountTexy;
+
+
+    public bool isPaused;
+
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        instance = this;
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerScript = player.GetComponent<PlayerController>();
+        spawnPosition = GameObject.FindGameObjectWithTag("Spawn Position");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Cancel") && !playerDeadMenu.activeSelf)
+        {
+            isPaused = !isPaused;
+            pauseMenu.SetActive(isPaused);
+
+            if (isPaused)
+            {
+                curserLockPause();
+            }
+            else
+            {
+                curserUnlockUnpause();
+            }
+        }
+    }
+
+    public void curserLockPause()
+    {
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void curserUnlockUnpause()
+    {
+        Time.timeScale = 1;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public IEnumerator playerDamage()
+    {
+        playerDamageFlash.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+        playerDamageFlash.SetActive(false);
+
+    }
+
+    public void checkEnemyTotal()
+    {
+        enemyNumber--;
+        enemyCountTexy.text = enemyNumber.ToString("F0");
+
+        if (enemyNumber <= 0)
+        {
+            winMenu.SetActive(true);
+            curserLockPause();
+        }
     }
 }
--- Terrific Crusaders Shooter/Assets/Scripts/PlayerController.cs
+++ Terrific Crusaders Shooter/Assets/Scripts/PlayerController.cs
@@ -4,32 +4,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
+
+    [Header("---Component---")]
     [SerializeField] CharacterController controller;
 
-    [SerializeField] float playerSpeed;
-    [SerializeField] float jumpHeight;
-    [SerializeField] float gravityValue;
-    [SerializeField] int jumpMax;
+    [Header("---Player Stats---")]
+    [Range(1, 5)] [SerializeField] float playerSpeed;
+    [Range(8, 15)] [SerializeField] float jumpHeight;
+    [Range(15, 35)] [SerializeField] float gravityValue;
+    [SerializeField] int jumpsMax;
+    [SerializeField] int HP;
 
+    [Header("---Gun Stats---")]
     [SerializeField] float shootRate;
     [SerializeField] int shootDist;
-    [SerializeField] GameObject cube;
+    [SerializeField] int shootDmg;
+    [SerializeField] GameObject gunModel;
+    [SerializeField] List<gunStats> gunStat = new List<gunStats>();
 
+    [SerializeField] GameObject cube;
     private Vector3 playerVelocity;
     private int timesJumped;
-    bool isShooting;
+    bool isShoot;
+    int selectedgun;
+    int hpOringal;
 
     private void Start()
     {
-        
+        Respawm();
     }
 
     void Update()
     {
         movement();
         StartCoroutine(shoot());
-
+        gunSelect();
     }
 
     void movement()
@@ -46,7 +55,7 @@ public class PlayerController : MonoBehaviour
         controller.Move(move * Time.deltaTime * playerSpeed);
 
         // Jumping
-        if (Input.GetButtonDown("Jump") && timesJumped < jumpMax)
+        if (Input.GetButtonDown("Jump") && timesJumped < jumpsMax)
         {
             timesJumped++;
             playerVelocity.y = jumpHeight;
@@ -60,26 +69,103 @@ public class PlayerController : MonoBehaviour
     //
     IEnumerator shoot()
     {
-        if(Input.GetButton("Shoot") && !isShooting)
+        if(Input.GetButton("Shoot") && !isShoot)
         {
-            isShooting = true;
+            isShoot = true;
+
             RaycastHit hit;
 
 
             if(Physics.Raycast(Camera.main.ViewportPointToRay(new Vector2(0.5f, 0.5f)),out hit, shootDist))
             {
+                if (hit.collider.GetComponent<IDamage>() != null)
+                {
+                    hit.collider.GetComponent<IDamage>().takeDamage(shootDmg);
+                }
+
                 //Creat Cube
                 //
-                //Instantiate(cube, hit.point, cube.transform.rotation);
+                Instantiate(cube, hit.point, cube.transform.rotation);
+
                 //
-                Instantiate(cube, hit.point, Camera.main.transform.rotation);
+                //Instantiate(cube, hit.point, Camera.main.transform.rotation);
+                //
+                //Instantiate(cube, hit.point, transform.parent.rotation);
             }
 
             yield return new WaitForSeconds(shootRate);
-            isShooting = false;
+            isShoot = false;
         }
-        
 
+    }
+
+    public void gunPickup(gunStats stats)
+    {
+        shootRate = stats.shootRate;
+        shootDist = stats.shootDist;
+        shootDmg = stats.shootDmg;
+        gunModel.GetComponent<MeshFilter>().sharedMesh = stats.gunModel.GetComponent<MeshFilter>().sharedMesh;
+        gunModel.GetComponent<MeshRenderer>().sharedMaterial = stats.gunModel.GetComponent<MeshRenderer>().sharedMaterial;
+
+        gunStat.Add(stats);
+    }
+
+    public void gunSelect()
+    {
+        if (gunStat.Count > 1)
+        {
+            if (Input.GetAxis("Mouse ScrollWheel") > 0 && selectedgun < gunStat.Count - 1)
+            {
+                selectedgun++;
+                changeGun();
+            }
+            else if (Input.GetAxis("Mouse ScrollWheel") < 0 && selectedgun > 0)
+            {
+                selectedgun--;
+                changeGun();
+            }
+        }
+    }
+
+    public void changeGun()
+    {
+        shootRate = gunStat[selectedgun].shootRate;
+        shootDist = gunStat[selectedgun].shootDist;
+        shootDmg = gunStat[selectedgun].shootDmg;
+
+        gunModel.GetComponent<MeshFilter>().sharedMesh = gunStat[selectedgun].gunModel.GetComponent<MeshFilter>().sharedMesh;
+        gunModel.GetComponent<MeshRenderer>().sharedMaterial = gunStat[selectedgun].gunModel.GetComponent<MeshRenderer>().sharedMaterial;
+
+    }
+
+    public void takeDamage(int dmg)
+    {
+        HP -= dmg;
+        updatePlayerHUD();
+
+        StartCoroutine(GameManager.instance.playerDamage());
+
+        if (HP <= 0)
+        {
+            GameManager.instance.playerDeadMenu.SetActive(true);
+            GameManager.instance.curserLockPause();
+
+        }
+    }
+
+    public void updatePlayerHUD()
+    {
+        GameManager.instance.playerHPBar.fillAmount = (float)HP / (float)hpOringal;
+    }
+
+    public void Respawm()
+    {
+        controller.enabled = false;
+        GameManager.instance.playerDeadMenu.SetActive(false);
+        HP = hpOringal;
+        updatePlayerHUD();
+        transform.position = GameManager.instance.spawnPosition.transform.position;
+        controller.enabled = true;
     }
 
 }
