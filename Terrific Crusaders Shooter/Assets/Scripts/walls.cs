0677141635e4e95e1849e7a0666e673c6aee0dca
Lmntlklr1 <bmelancon1023@gmail.com>
2022-10-10 20:59:11 -0400

    Breakable and unbreakable walls with materials

---
+++ Terrific Crusaders Shooter/Assets/Scripts/walls.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walls : MonoBehaviour, IDamage
+{
+    [SerializeField] bool penetrable;
+    [SerializeField] GameObject wall;
+    int HP = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void takeDamage(int DMG)
+    {
+        if (penetrable)
+        {
+            HP -= DMG;
+            if (HP <= 0)
+            {
+                Destroy(wall);
+            }
+        }
+    }
+}
