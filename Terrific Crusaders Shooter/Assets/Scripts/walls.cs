22dd7f393a938d2f13844c4d0237376c4be8a4d7
Lmntlklr1 <bmelancon1023@gmail.com>
2022-10-11 10:02:52 -0400

    Enemy stats and gun stats balancing

--- Terrific Crusaders Shooter/Assets/Scripts/walls.cs
+++ Terrific Crusaders Shooter/Assets/Scripts/walls.cs
@@ -6,7 +6,7 @@ public class walls : MonoBehaviour, IDamage
 {
     [SerializeField] bool penetrable;
     [SerializeField] GameObject wall;
-    int HP = 10;
+    int HP = 6;
     // Start is called before the first frame update
     void Start()
     {
