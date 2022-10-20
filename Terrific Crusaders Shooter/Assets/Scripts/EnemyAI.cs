bb630fc34088801906196808bd5350ee722c9b9d
Nevin T Whitlock <NTWhitlock@student.fullsail.edu>
2022-10-20 10:09:53 -0500

    Removed commented out code and a left over debug
    Clean up update

--- Terrific Crusaders Shooter/Assets/Scripts/EnemyAI.cs
+++ Terrific Crusaders Shooter/Assets/Scripts/EnemyAI.cs
@@ -51,7 +51,7 @@ public class EnemyAI : MonoBehaviour, IDamage
     float patrolSpeed;
     void Start()
     {
-        //GameManager.instance.enemyAmount++;
+       
         stoppingDistanceOrgin = agent.stoppingDistance;
         startingPosition = transform.position;
         patrolSpeed = agent.speed;
