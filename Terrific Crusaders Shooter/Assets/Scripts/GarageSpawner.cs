ce6c890eb588ad91968f10419e88f026f3b6e18b
PennyWizard <104772721+PennyWizard@users.noreply.github.com>
2022-10-19 21:45:48 -0400

    door spawner

---
+++ Terrific Crusaders Shooter/Assets/Scripts/GarageSpawner.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarageSpawner : MonoBehaviour, IDamage
+{
+    [SerializeField] bool penetrable;
+    [SerializeField] GameObject wall;
+
+    [SerializeField] GameObject enemy;
+    [SerializeField] int maxEnemies = 2;
+
+    int HP = 1;
+    int enemiesSpawned;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.instance.enemyAmount += maxEnemies;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void takeDamage(int DMG)
+    {
+        if (penetrable)
+        {
+            HP -= DMG;
+            if (HP <= 0)
+            {
+
+                Instantiate(enemy, new Vector3(-163.93f, 3, 108.78f), Quaternion.identity);
+                Instantiate(enemy, new Vector3(-163.93f, 3, 98.59f), Quaternion.identity);
+                Instantiate(enemy, new Vector3(-156.93f, 3, 98.59f), Quaternion.identity);
+                Instantiate(enemy, new Vector3(-156.93f, 3, 104.77f), Quaternion.identity);
+                enemiesSpawned++;
+
+
+                Destroy(wall);
+            }
+        }
+    }
+}
---
+++ Terrific Crusaders Shooter/Assets/Scripts/StudySpawner.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StudySpawner : MonoBehaviour, IDamage
+{
+    [SerializeField] bool penetrable;
+    [SerializeField] GameObject wall;
+
+    [SerializeField] GameObject enemy;
+    [SerializeField] GameObject enemy2;
+    [SerializeField] int maxEnemies = 2;
+
+    int HP = 1;
+    int enemiesSpawned;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.instance.enemyAmount += maxEnemies;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void takeDamage(int DMG)
+    {
+        if (penetrable)
+        {
+            HP -= DMG;
+            if (HP <= 0)
+            {
+
+                Instantiate(enemy, new Vector3(4.2f, 2, 7.11f), Quaternion.identity);
+                Instantiate(enemy2, new Vector3(5.2f, 2, 8.11f), Quaternion.identity);
+                enemiesSpawned++;
+
+
+                Destroy(wall);
+            }
+        }
+    }
+}
