469644bec97e4c03bb72f34f9110e8a01f0903d2
Nevin T Whitlock <104774158+NTWhitlock@users.noreply.github.com>
2022-10-31 20:03:18 -0400

    Added gun to enemy
    setting up a shooting position

--- Terrific Crusaders Shooter/Assets/Scripts/States/chaseState.cs
+++ Terrific Crusaders Shooter/Assets/Scripts/States/chaseState.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class chaseState : MonoBehaviour
+public class chaseState : State
 {
-    // Start is called before the first frame update
-    void Start()
+    public override State runCurrentState()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        return this;
     }
 }
