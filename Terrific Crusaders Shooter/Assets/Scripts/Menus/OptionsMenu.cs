2f4537643b8a46091d9eb4be6781f108f3a0d076
PennyWizard <104772721+PennyWizard@users.noreply.github.com>
2022-11-01 13:32:10 -0400

    Sound controlls working

--- Terrific Crusaders Shooter/Assets/Scripts/Menus/OptionsMenu.cs
+++ Terrific Crusaders Shooter/Assets/Scripts/Menus/OptionsMenu.cs
@@ -2,14 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
 
 public class OptionsMenu : MonoBehaviour
 {
-    public AudioMixer SFX;
+    public AudioMixer SFX, music;
+    public Slider sfxSlider, musicSlider;
 
-    public void SetVolume(float volume)
+    //public TMP_Text sfxLabel, musicLabel;
+
+    public void SetSFXVolume()
     {
-        SFX.SetFloat("sfxVolume", volume);
+        SFX.SetFloat("sfxVolume", sfxSlider.value);
+    }
+
+    public void SetMusicVolume()
+    {
+        music.SetFloat("musicVolume", musicSlider.value);
     }
 
     public void Invert(bool isInverted)
