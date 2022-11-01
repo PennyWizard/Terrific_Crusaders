fa9fcedf8f676b56290d53da9f18877e503d4667
Nevin T Whitlock <NTWhitlock@student.fullsail.edu>
2022-11-01 16:48:54 -0500

    Still trying to get AI to work
    cant get AI to search for player after it is shot or loses to player

--- Terrific Crusaders Shooter/Assets/Scripts/AI hearing/Sound.cs
+++ Terrific Crusaders Shooter/Assets/Scripts/AI hearing/Sound.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shound : MonoBehaviour
+public class Sound : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
