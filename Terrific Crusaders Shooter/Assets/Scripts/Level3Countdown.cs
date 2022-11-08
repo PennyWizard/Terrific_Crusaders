6bc4b59fdb3e6e9bb2d8ac7d9cddc9c95ceadaf4
PennyWizard <104772721+PennyWizard@users.noreply.github.com>
2022-11-08 13:06:52 -0500

    level 3 countdown

---
+++ Terrific Crusaders Shooter/Assets/Scripts/Level3Countdown.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Level3Countdown : MonoBehaviour
+{
+    private float currentTime;
+    public float startTime;
+    public Text countDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countDown.text = currentTime.ToString("0");
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+            GameManager.instance.Menu.SetActive(true);
+            GameManager.instance.respawnButton.SetActive(true);
+            GameManager.instance.curserLock();
+            GameManager.instance.isMenuOpen = true;
+        }
+    }
+
+
+}
